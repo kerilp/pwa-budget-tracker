6a8c43a92765c8a52809a1076e01ef7c54f6a9d4
Keri Price <keri.lynne.p@gmail.com>
2021-10-18 11:19:47 -0400

    updated db js and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Offline Budget Tracker
+
+Adds offline capability to existing Budget Tracker application, making a Progressive Web Application.
+
+## Links
+
+[Deployed to Heroku](https://dry-badlands-47800.herokuapp.com/)
+
+[Github Repository](https://github.com/kerilp/pwa-budget-tracker)
+
+## Screenshots
+
+![Screenshot](./public/icons/screenshot.png)
+
+## Contact
+
+Email: [klprice128@gmail.com](mailto:klprice128@gmail.com)
+
+GitHub: [https://github.com/kerilp](https://github.com/kerilp)
---
+++ public/indexDb.js
@@ -0,0 +1,47 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+const checkDatabase = () => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "text/plain, application/json, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+};
+
+window.addEventListener("online", checkDatabase);
